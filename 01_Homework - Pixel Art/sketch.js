3c63d4d00427fc817a4c54d46e9d62a6b45970e3
(s) Rosie Dine <rdine@pm7-0087.uopnet.plymouth.ac.uk>
2018-10-30 17:32:07 +0000

    Homework

---
+++ 01_Homework - Pixel Art/sketch.js
@@ -0,0 +1,298 @@
+function setup() {
+  // Create the canvas
+  createCanvas(800, 600);
+  background(250);
+
+//ShapeOutline
+
+fill(0);
+
+
+ // A rectangle
+ rect(395, 50, 30, 30);
+ // A rectangle
+ rect(364, 50, 30, 30);
+ // A rectangle
+ rect(333, 50, 30, 30);
+ // A rectangle
+ rect(302, 50, 30, 30);
+ // A rectangle
+ rect(271, 50, 30, 30);
+ // A rectangle
+ rect(240, 50, 30, 30);
+
+ // A rectangle
+ rect(240, 81, 30, 30);
+ // A rectangle
+ rect(395, 81, 30, 30);
+
+ // A rectangle
+ rect(209, 81, 30, 30);
+ // A rectangle
+ rect(426, 81, 30, 30);
+
+ // A rectangle
+ rect(178, 81, 30, 30);
+ // A rectangle
+ rect(457, 81, 30, 30);
+
+ // A rectangle
+ rect(178, 112, 30, 30);
+ // A rectangle
+ rect(457, 112, 30, 30);
+
+ // A rectangle
+ rect(147, 112, 30, 30);
+ // A rectangle
+ rect(488, 112, 30, 30);
+
+ // A rectangle
+ rect(147, 143, 30, 30);
+ // A rectangle
+ rect(488, 143, 30, 30);
+
+ // A rectangle
+ rect(116, 143, 30, 30);
+ // A rectangle
+ rect(519, 143, 30, 30);
+
+ // A rectangle
+ rect(116, 174, 30, 30);
+ // A rectangle
+ rect(116, 205, 30, 30);
+ // A rectangle
+ rect(519, 174, 30, 30);
+ // A rectangle
+ rect(519, 205, 30, 30);
+
+ // A rectangle
+ rect(85, 205, 30, 30);
+ // A rectangle
+ rect(550, 205, 30, 30);
+
+ // A rectangle
+ rect(85, 236, 30, 30);
+ // A rectangle
+ rect(85, 267, 30, 30);
+ // A rectangle
+ rect(85, 298, 30, 30);
+ // A rectangle
+ rect(85, 329, 30, 30);
+ // A rectangle
+ rect(85, 360, 30, 30);
+ // A rectangle
+ rect(85, 391, 30, 30);
+
+ // A rectangle
+ rect(550, 236, 30, 30);
+ // A rectangle
+ rect(550, 267, 30, 30);
+ // A rectangle
+ rect(550, 298, 30, 30);
+ // A rectangle
+ rect(550, 329, 30, 30);
+ // A rectangle
+ rect(550, 360, 30, 30);
+ // A rectangle
+ rect(550, 391, 30, 30);
+
+ // A rectangle
+ rect(116, 391, 30, 30);
+ // A rectangle
+ rect(147, 391, 30, 30);
+ // A rectangle
+ rect(178, 391, 30, 30);
+
+ // A rectangle
+ rect(519, 391, 30, 30);
+ // A rectangle
+ rect(488, 391, 30, 30);
+ // A rectangle
+ rect(457, 391, 30, 30);
+
+ // A rectangle
+ rect(178, 360, 30, 30);
+ // A rectangle
+ rect(457, 360, 30, 30);
+
+ // A rectangle
+ rect(209, 360, 30, 30);
+ // A rectangle
+ rect(240, 360, 30, 30);
+ // A rectangle
+ rect(271, 360, 30, 30);
+ // A rectangle
+ rect(302, 360, 30, 30);
+ // A rectangle
+ rect(333, 360, 30, 30);
+ // A rectangle
+ rect(364, 360, 30, 30);
+ // A rectangle
+ rect(395, 360, 30, 30);
+ // A rectangle
+ rect(426, 360, 30, 30);
+
+ // A rectangle
+ rect(271, 391, 30, 30);
+ // A rectangle
+ rect(271, 422, 30, 30);
+
+ // A rectangle
+ rect(364, 391, 30, 30);
+ // A rectangle
+ rect(364, 422, 30, 30);
+
+
+ // A rectangle
+ rect(116, 422, 30, 30);
+ // A rectangle
+ rect(147, 422, 30, 30);
+
+
+ // A rectangle
+ rect(519, 422, 30, 30);
+ // A rectangle
+ rect(488, 422, 30, 30);
+
+
+ // A rectangle
+ rect(147, 453, 30, 30);
+ // A rectangle
+ rect(147, 484, 30, 30);
+ // A rectangle
+ rect(178, 484, 30, 30);
+
+
+ // A rectangle
+ rect(178, 515, 30, 30);
+ // A rectangle
+ rect(209, 515, 30, 30);
+ // A rectangle
+ rect(240, 515, 30, 30);
+ // A rectangle
+ rect(271, 515, 30, 30);
+ // A rectangle
+ rect(302, 515, 30, 30);
+ // A rectangle
+ rect(333, 515, 30, 30);
+ // A rectangle
+ rect(364, 515, 30, 30);
+ // A rectangle
+ rect(394, 515, 30, 30);
+ // A rectangle
+ rect(425, 515, 30, 30);
+ // A rectangle
+ rect(456, 515, 30, 30);
+
+ // A rectangle
+ rect(488, 453, 30, 30);
+ // A rectangle
+ rect(488, 484, 30, 30);
+ // A rectangle
+ rect(457, 484, 30, 30);
+
+//Inside Colour (Green)
+
+fill(44, 250, 100, 200);
+stroke(44, 250, 100, 200);
+
+ // A rectangle
+ rect(333, 81, 30, 30);
+ // A rectangle
+ rect(302, 81, 30, 30);
+
+ // A rectangle
+ rect(333, 112, 30, 30);
+ // A rectangle
+ rect(302, 112, 30, 30);
+
+ // A rectangle
+ rect(333, 143, 30, 30);
+ // A rectangle
+ rect(302, 143, 30, 30);
+
+ // A rectangle
+ rect(333, 174, 30, 30);
+ // A rectangle
+ rect(302, 174, 30, 30);
+
+ // A rectangle
+ rect(364, 143, 30, 30);
+ // A rectangle
+ rect(271, 143, 30, 30);
+
+ // A rectangle
+ rect(364, 174, 30, 30);
+ // A rectangle
+ rect(271, 174, 30, 30);
+
+ // A rectangle
+ rect(240, 174, 30, 30);
+ // A rectangle
+ rect(209, 174, 30, 30);
+ // A rectangle
+ rect(178, 174, 30, 30);
+ // A rectangle
+ rect(178, 143, 30, 30);
+
+ // A rectangle
+ rect(395, 174, 30, 30);
+ // A rectangle
+ rect(426, 174, 30, 30);
+ // A rectangle
+ rect(457, 174, 30, 30);
+ // A rectangle
+ rect(457, 143, 30, 30);
+
+ // A rectangle
+ rect(209, 205, 30, 154);
+ // A rectangle
+ rect(426, 205, 30, 154);
+
+ // A rectangle
+ rect(488, 360, 30, 30);
+ // A rectangle
+ rect(519, 360, 30, 30);
+
+ // A rectangle
+ rect(488, 329, 30, 30);
+ // A rectangle
+ rect(519, 329, 30, 30);
+ // A rectangle
+ rect(457, 329, 30, 30);
+
+ // A rectangle
+ rect(178, 329, 30, 30);
+ // A rectangle
+ rect(147, 329, 30, 30);
+ // A rectangle
+ rect(116, 329, 30, 30);
+
+ // A rectangle
+ rect(147, 360, 30, 30);
+ // A rectangle
+ rect(116, 360, 30, 30);
+
+ // A rectangle
+ rect(240, 205, 30, 30);
+ // A rectangle
+ rect(395, 205, 30, 30);
+
+ // A rectangle
+ rect(178, 298, 30, 30);
+ // A rectangle
+ rect(457, 298, 30, 30);
+
+ // A rectangle
+ rect(240, 329, 30, 30);
+ // A rectangle
+ rect(395, 329, 30, 30);
+
+
+
+
+
+
+
+
+}

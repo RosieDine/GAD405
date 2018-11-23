4646f0a1f11ca4a5373fdcfdaf78743113c449d9
(s) Rosie Dine <rdine@pm7-0087.uopnet.plymouth.ac.uk>
2018-11-23 13:03:16 +0000

    Mitosis update, added push, splice, more functions,and made cells split

--- 10_Assignment_One/sketch.js
+++ 10_Assignment_One/sketch.js
@@ -23,64 +23,70 @@
 
 //  }
 //}
-var cells = [];
 
-//Initalisation function
-function setup() {
-    //This sets the size of the rendering window
-  createCanvas(594, 841);
-  //Cell (capitalised) is the name of the constructor function that makes cell objects
-  //Cells (plural) is the array holding a list of cell objects.
-  cells.push(new Cell());
+var cells = []; //This array is a list of data. In this case, "cells". It is identified by an index number representing its position.
+
+function setup() { //Initalisation function
+  createCanvas(541, 841); //Sets the size of the rendering window
+	cells.push(new Cell()); //Cell (capitalised) is the name of the constructor function that makes cell objects
+  cells.push(new Cell()); //Cells (plural) is the array holding a list of cell objects.
 }
 
-//Rendering function
-function draw() {
-  background(0);
-  for (var i = 0; i < cells.length; i++) {
-    cells[i].move();
-    cells[i].show();
-  }
+function draw() { //Rendering function
+  background(80);
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].move();
+		cells[i].show();
+	}
 }
-//Global event that detects if the user has pressed the mouse
-function mousePressed(){
-  for (var i = 0; i < cells.length; i++) {
-    if (cells[i].clicked(mouseX, mouseY)) {
 
-    }
-  }
-
-//This particular constructor function represents a cell object
-function cell() {
-  //createVector stores the objects position (with random height and width)
-  this.pos = createVector(random(width), random(height));
-  //When the cells split, the input number will be their radius
-  this.r = 80;
-  //This is the colour of the cell
-  this.c = colour(random(100, 255), 0, random(100, 255));
-
-  this.clicked = function(x, y) {
-    var d = dist(this.pos.x, this.pos.y, x, y);
-    if (d < this.r) {
-      return true;
-    } else {
-      return false;
-    }
-
-  }
-
-  //This will allow the cells to move around the screen with a random varaible
-  //(named velocity) value
-  this.move = function() {
-    var vel = p5.Vector.random2D();
-    this.pos.add(vel);
-  }
-
-  this.show = function() {
-    //There will be no outline around the shape
-    noStroke();
-    fill(this.c);
-    ellipse(this.pos.x, this.pos.y, this.pos.r, this.pos.r)
-    }
-  }
+function mousePressed() { //Global event that detects if the user has pressed the mouse
+	for (var i = cells.length-1; i >= 0; i--) {
+		if (cells[i].clicked(mouseX, mouseY)) {
+			cells.push(cells[i].mitosis());
+			cells.push(cells[i].mitosis());
+			//If the cells are clicked on, code will make two new cells from the mitosis function
+			//console.log("CLICKED");
+			cells.splice(i, 1); //The moment the [i] cell is clicked, it will be deleted and break down into two new cells
+		}
+	}
 }
+
+function Cell(pos, r, c)  { //This particular constructor function represents a cell object
+
+	if (pos) {
+		this.pos = pos.copy();
+	} else {
+		this.pos = createVector(random(width), random(height));
+	}
+
+	this.r = r || 60; //Radius
+	this.c = c || color(random(100, 255), 0, random(100, 255)); //Colour of the cell
+
+	this.clicked = function(x, y) {
+		var d = dist(this.pos.x, this.pos.y, x, y);
+		if (d < this.r) { //If the distance is less than the radius, then the cell has been clicked
+				return true;
+				} else {
+				return false;
+		}
+	}
+
+	this.mitosis = function () { //This function will help with the act of splitting the cells
+		this.pos.x += random(-this.r, this.r); //Before the new cell is made, the position will be altered randomly based on the radius variable
+		var cell = new Cell(this.pos, this.r*0.8, this.c); //A new cell will be made at a specific position, with radius multiplied by 0.8 and the variable assigned colour
+		return cell;
+	}
+
+	this.move = function() { //This function will allow the cells to 'shake' in the rendering window
+		var vel = p5.Vector.random2D();
+		this.pos.add(vel);
+	}
+
+	this.show = function() {
+		noStroke(); //There will be no outline around the shape
+		fill(this.c);
+		ellipse(this.pos.x, this.pos.y, this.r, this.r)
+
+			}
+		}
